f1089dce11971ab4582df8d4d30260f5fe5ace87
vitsarutnan <vitsarut.n@hotmail.com>
2020-05-13 16:08:31 +0700

    Change to react

---
+++ precache-manifest.a91f29508319bb2ba9a7f8ad22f672bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a2ef4b4dd96cd783833cf793567234a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c49f38f534d5ba82ec41",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9662ebf8fdaa54da8318",
+    "url": "/static/js/2.9a5a9f82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9a5a9f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c49f38f534d5ba82ec41",
+    "url": "/static/js/main.103a6fab.chunk.js"
+  },
+  {
+    "revision": "34cc9e86b84fa9d9da1c",
+    "url": "/static/js/runtime-main.5ce9eb8a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
